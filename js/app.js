f9c9c8d03d70cf2b5e4f40cb1fe7df7c8d9f9770
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-01-06 18:09:50 -0500

    Third Commit - Version 1.2. This consist of everything from version 1.1. Removed white spaces and added comments to the code to bring standard coding practice. Added display and flex-wrap properties for navigation bar menu item. Introduced media query for unordered list to resize to flex format of rows when min-width of screen is 50em. Else the flex format is set to column and align-items are set to stretch

--- js/app.js
+++ js/app.js
@@ -1,85 +1,71 @@
+/* Declare constants for the elements */
+
 const $NAVIGATION_BAR = document.querySelector('.navbar__menu');
 const $ANCHOR_TAG = document.getElementsByClassName('.menu__link');
-const $SECTION_LIST = document.querySelectorAll("section")
-
+const $SECTION_LIST = document.querySelectorAll("section");
 const $TOP_ARROW = document.querySelector('.arrow-top');
 const $UNORDERED_LIST = document.querySelector('#navbar__list');
-
 const fragment = document.createDocumentFragment();
 
-//Create Dynamic List Anchor Tag Elements.
+/* Create Dynamic List Anchor Tag Elements. */
+
 $SECTION_LIST.forEach((section)=>{
+    /* Create List and Anchor Tags */
 
     let listElement = document.createElement('li');
     let anchorElement = document.createElement('a');
+
+    /* Set Attribute values for anchor tag and append in the list tag */
 
     anchorElement.setAttribute('id',`nav-${section.id}`);
     anchorElement.setAttribute('href',`#${section.id}`);
     anchorElement.setAttribute('class','menu__link');
     anchorElement.textContent = section.querySelector('h2').textContent;
     listElement.appendChild(anchorElement);
+
+    /* Add the newly created list tag to the fragment object */
+
     fragment.appendChild(listElement);
-    });
+});
+
+
+/* After the section array is parsed and all elements are created and appended in the fragment, add the fragment to the unordered list */
 
 $UNORDERED_LIST.appendChild(fragment);
 
-const hoverStatusFunction = (event)=>{
-   if(event.target.classList.contains('menu__link')&&!event.target.classList.contains('menu__link__change'))
-   {
-       event.target.classList.toggle('menu__link__change');
-   }
+/* Helper Function to verify if section is active */
 
-   else if(event.target.classList.contains('menu__link')&& event.target.classList.contains('menu__link__change'))
-   {
-       event.target.classList.toggle('menu__link__change');
-   }
+const isSectionActive = (element)=> {
 
-   
+    const {top,bottom} = element.getBoundingClientRect();
     
+        if(top<=90 && bottom>=100)
+        {
+            return true;
+        }
+        
 }
 
-const isSectionActive = (element)=>{
-    // return element.getBoundClientRect().bottom-element.getBoundClientRect().top==element.getBoundClientRect().height?true:false;
-    // return element.getBoundingClientRect().bottom<=10 && element.getBoundingClientRect().bottom<element.getBoundingClientRect().height
-    // return element.getBoundingClientRect().top<window.innerHeight;
-const {top,bottom} = element.getBoundingClientRect();
+/* Listener Event Handler to highlight section that is active */
 
-    // if(element.getBoundingClientRect().top<=10 && element.getBoundingClientRect().top<window.innerHeight)
-    if(
-        // top<=element.offesetTop && bottom<=height
-        // top<=150 && bottom>=150
-        top<=90 && bottom>=100
-    )
-    {
-
-    //    console.log(`${element.id} Top Value is ${top}`);
-    //    console.log(`${element.id} Bottom Value is ${bottom}`);
-    //    console.log(`Window Height is ${window.innerHeight}`);
-    // console.log(`${element.id} Top value is ${top}`);
-    // console.log(`${element.id} Bottom value is ${bottom}`);
-    // console.log(`${element.id} Height value is ${height}`);
-    // console.log(`Condition is ${top<=0 && bottom-top<=height}`)
-    return true;
-    }
-    
-}
-
-
-const highlightScrollSection = ()=>{
+const highlightScrollSection = ()=> {
 
     for (const section of $SECTION_LIST)
     {
-        const navItem = document.querySelector(`#nav-${section.id}`)
-        if(isSectionActive(section)){
-            section.classList.add('active-background')
-            navItem.classList.add('active-nav')
-            
+        const navItem = document.querySelector(`#nav-${section.id}`);
 
+        /* Verify if section is active with the helper function */
+
+        if(isSectionActive(section))
+        {
+            section.classList.add('active-background');
+            navItem.classList.add('active-nav');
         }
 
-        else {
+        else
+        {
             section.classList.remove('active-background');
-            navItem.classList.remove('active-nav')
+            navItem.classList.remove('active-nav');
             
         }
     }
@@ -87,24 +73,28 @@ const highlightScrollSection = ()=>{
 
     if(window.scrollY>0)
     {
-        // $HEADER_BAR.classList.add('sticky');
-        $NAVIGATION_BAR.classList.add('sticky');
+        $NAVIGATION_BAR.classList.add('header__color');
         $TOP_ARROW.classList.add('active');
     }
 
     else 
     {
-        $NAVIGATION_BAR.classList.remove('sticky');
+        $NAVIGATION_BAR.classList.remove('header__color');
         $TOP_ARROW.classList.remove('active');
     }
 
 
 }
 
-const moveWindowUp = ()=>{
-    window.scrollTo({top:0,behaviro:'smooth'})
+/* Listener Event Handler to move the scrollbar to top */
+
+const moveWindowUp = ()=> {
+
+    window.scrollTo({top:0,behaviro:'smooth'});
+
 }
 
-window.addEventListener('scroll',highlightScrollSection);
+/* Adding Listener Events */
 
+window.addEventListener('scroll',highlightScrollSection);
 $TOP_ARROW.addEventListener('click',moveWindowUp);
