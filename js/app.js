3cb637401b4c4038b02712a8482f5d64e7054a30
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-04-06 17:26:32 -0400

    Nineth Commit- Version 1.8 This version contains changes to the javascript code, html and style.css to add the navigation menu for smaller screen

--- js/app.js
+++ js/app.js
@@ -4,7 +4,11 @@ const $NAVIGATION_BAR = document.querySelector('.navbar__menu');
 const $SECTION_LIST = document.querySelectorAll("section");
 const $TOP_ARROW = document.querySelector('.arrow-top');
 const $UNORDERED_LIST = document.querySelector('#navbar__list');
+const $UNORDERED_LIST_MOBILE = document.querySelector('#navbar__list__mobile');
+const $NAVIGATION_BURGER = document.querySelector('.navigation__burger')
+const $NAVIGATION_MODAL = document.querySelector('.navigation__modal');
 const fragment = document.createDocumentFragment();
+const fragment_mobile = document.createDocumentFragment();
 
 /* Create Dynamic List Anchor Tag Elements. */
 
@@ -28,9 +32,35 @@ $SECTION_LIST.forEach((section)=>{
     
 });
 
-/* After the section array is parsed and all elements are created and appended in the fragment, add the fragment to the unordered list */
+/* A separate forEach loop is ran to create a separate fragment for mobile version as the fragment does not have a parent node to remove and append to the new child node
+The appendChild ensures the node object is part of one parent at one time. Hence the fragment code above cannot be used to append both $UNORDERED_LIST and $UNORDERED_LIST_MOBILE
+version.
+Refer to the MDN documentation for appendChild and DocumentFragment for further details.
+ */
 
+$SECTION_LIST.forEach((section)=>{
+
+    /* Create List and Anchor Tags */
+
+    let listElement = document.createElement('li');
+    let anchorElement = document.createElement('a');
+
+    /* Set Attribute values for anchor tag and append in the list tag */
+
+    anchorElement.setAttribute('id',`nav-mobile-${section.id}`);
+    anchorElement.setAttribute('class','menu__link');
+    anchorElement.textContent = section.querySelector('h2').textContent;
+    listElement.appendChild(anchorElement);
+
+    /* Add the newly created list tag to the fragment object */
+
+    fragment_mobile.appendChild(listElement);
+    
+});
+
+/* After the section array is parsed and all elements are created and appended in the fragment, add the fragment to the unordered list */
 $UNORDERED_LIST.appendChild(fragment);
+$UNORDERED_LIST_MOBILE.appendChild(fragment_mobile);
 
 /* Helper Function to verify if section is active */
 
@@ -52,6 +82,7 @@ const highlightScrollSection = ()=> {
     for (const section of $SECTION_LIST)
     {
         const navItem = document.querySelector(`#nav-${section.id}`);
+        const navItemMobile = document.querySelector(`#nav-mobile-${section.id}`);
 
         /* Verify if section is active with the helper function */
 
@@ -59,12 +90,18 @@ const highlightScrollSection = ()=> {
         {
             section.classList.add('active-background');
             navItem.classList.add('active-nav');
+            navItemMobile.classList.add('active-nav');
+
+            
         }
 
         else
         {
             section.classList.remove('active-background');
             navItem.classList.remove('active-nav');
+            navItemMobile.classList.remove('active-nav');
+            
+            
             
         }
     }
@@ -105,8 +142,28 @@ const moveWindowUp = ()=> {
 
 }
 
+/* Listener Event Handler to open the navigation-modal for small screen resolution */
+
+const slideNavigationModal = ()=>{
+
+    $NAVIGATION_MODAL.classList.add('navigation__modal__display');
+    
+}
+
+const closeNavigationModal = (e)=>{
+
+    if(e.target.nodeName!=='A' || e.target.nodeName==='SPAN')
+    {
+        $NAVIGATION_MODAL.classList.remove('navigation__modal__display');
+    }
+    
+}
+
 /* Adding Listener Events */
 
 window.addEventListener('scroll',highlightScrollSection);
 $UNORDERED_LIST.addEventListener('click',scrollIntoSection);
+$UNORDERED_LIST_MOBILE.addEventListener('click',scrollIntoSection);
 $TOP_ARROW.addEventListener('click',moveWindowUp);
+$NAVIGATION_BURGER.addEventListener('click',slideNavigationModal);
+$NAVIGATION_MODAL.addEventListener('click',closeNavigationModal);
